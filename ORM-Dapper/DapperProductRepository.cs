d5d309e439801e575c456be52875416fd08b92e0
Chinedu Obinnah~ <cobinnah@yahoo.com>
2023-06-17 11:56:39 -0500

    Dapper Complete

---
+++ ORM-Dapper/DapperProductRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Dapper;
+
+namespace ORM_Dapper
+{
+    public class DapperProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public DapperProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM products;");
+        }
+
+        //public Product GetProduct(int id)
+        //{
+        //    return _conn.QuerySingle<Product>("SELECT * FROM products WHERE ProductID = @id;",
+        //        new { id =  id });
+        //}
+
+        public void UpdatedProduct(Product produtct)
+        {
+            _conn.Execute("UPDATE product SET " +
+                          "Name = @name, " +
+                          "Price = @price, " +
+                          "CategoryID =@catID," +
+                          " OnSale = @onSale," +
+                          "StockLevel= @stock" +
+                          " WHERE ProductID = @id,);",
+                          new {
+                            
+                         id = produtct.ProductID,
+                         name = produtct.Name,
+                         price = produtct.Price,
+                         catID = produtct.CategoryID,
+                         onSale = produtct.OnSale,
+                          stock = produtct.StockLevel
+                         });
+           
+        }
+    }
+}
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace ORM_Dapper
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using ORM_Dapper;
+using System.Data;
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+string connString = config.GetConnectionString("DefaultConnection");
+
+IDbConnection conn = new MySqlConnection(connString);
+
+//var departmentRepo = new DapperDepartmentRepository(conn);
+
+//departmentRepo.InsertDepartment("New Department");
+
+//var departments = departmentRepo.GetAllDepartments();
+
+//foreach (var department in departments)
+//{
+//    Console.WriteLine(department.DepartmentID);
+//    Console.WriteLine(department.Name);
+//    Console.WriteLine();
+//    Console.WriteLine();
+//}
+
+var productRepository = new DapperProductRepository(conn);
+
+//var productToUpdate = productRepository.GetProduct(800);
+
+//productToUpdate.Name = "UPDATED!!!";
+//productToUpdate.OnSale = false;
+//productToUpdate.CategoryID = 1;
+//productToUpdate.StockLevel = 1000;
+//productToUpdate.Price = 12.99;
+
+
+//productRepository.UpdatedProduct(productToUpdate);
+
+
+ var products = productRepository.GetAllProducts();
+foreach (var product in products)
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    Console.WriteLine(product.Name);
+    Console.WriteLine(product.ProductID);
+    Console.WriteLine(product.Price);
+    Console.WriteLine(product.OnSale);
+    Console.WriteLine(product.CategoryID);
+    Console.WriteLine(product.StockLevel);
+    Console.WriteLine();
+    Console.WriteLine();
+
 }
